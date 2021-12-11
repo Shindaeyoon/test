ef01be17c134a436d40d8868edf0eb5f46e788b1
unknown <sibaguyn@gmail.com>
2021-12-11 12:32:44 +0900

    2021 12 11

---
+++ 소스.c
@@ -0,0 +1,52 @@
+/*
+#include <stdio.h> //<p.46 문제1>
+
+int main(void)
+{
+	printf("제 이름은 홍길동입니다. \n");
+	printf("나이는 %d이고요. \n", 20);
+	printf("제가 사는 곳의 번지수는 %d-%d입니다. \n", 123, 456);
+	
+	return 0;
+}
+*/
+/*
+#include <stdio.h> //<p.46 문제2>
+
+int main(void)
+{
+	printf("%d×%d=%d \n", 4, 5, 4*5);
+	printf("%d×%d=%d \n", 7, 9, 7*9);
+
+	return 0;
+}
+*/
+
+/*
+#include <stdio.h> //<p.74 문제1>
+
+int main(void)
+{
+	int num1, num2, result, result2;
+	printf("두 개의 정수를 입력하세요. :");
+	scanf_s("%d %d", &num1,&num2);
+	result = num1 * num2;
+	result2 = num1 - num2;
+	printf("%d × %d = %d \n", num1, num2, result);
+	printf("%d - %d = %d \n", num1, num2, result2);
+	return 0;
+}
+*/
+/*
+#include <stdio.h> //<p.74 문제2>
+
+int main(void)
+{
+	int num1, num2, num3, result;
+	printf("세 개의 정수를 입력하세요. :");
+	scanf_s("%d %d %d", &num1, &num2, &num3);
+	result = num1 * num2 + num3;
+	printf("%d × %d + %d = %d", num1, num2, num3, result);
+	return 0;
+}
+*/
